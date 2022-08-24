b116c12aa9dc26750f5377d69e8cb2638175555b
ThiagoGouveia <thiago.rodrigues.gouveia@gmail.com>
2022-08-24 12:19:08 -0300

    rotas e controllers

--- ps-inpeace/routes/web.php
+++ ps-inpeace/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/igrejas', '\App\Http\Controllers\IgrejasController');
+Route::resource('/membros', '\App\Http\Controllers\MembrosController');
